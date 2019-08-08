d35d9ab9d92c742994c75714ce1ed395ee6f33ac
Hans <hans@wisy.app>
2019-08-07 23:51:36 -0500

    using workbox

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('app-shell').then(cache => {
-      return cache.add('/');
-    })
-  );
-});
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+);
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+workbox.routing.registerRoute('/', new workbox.strategies.NetworkFirst());
+
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open('app-shell').then(cache => {
+//       return cache.add('/');
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
