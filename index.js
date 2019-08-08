119db9b534457d6292d8cd792bc66cbb4463a3b2
Hans <hans@wisy.app>
2019-08-07 23:02:54 -0500

    service worker

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(registration => {
+      console.log(registration);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
